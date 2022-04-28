3c676db3deb940fa2f42472f72495929462736da
Park Jineon <79782666+parkjineon@users.noreply.github.com>
2022-04-28 13:49:02 +0900

    Create 사칙연산.py
    두 자연수를 받아, 합과 차,곱,몫,나머지 출력하기.

---
+++ 단계별로 풀어보기/입출력과 사칙연산/9단계/사칙연산.py
@@ -0,0 +1,12 @@
+def main():
+    a=input().split(' ')
+    a[0]=int(a[0])
+    a[1]=int(a[1])
+    print(a[0]+a[1])
+    print(a[0]-a[1])
+    print(a[0]*a[1])
+    print(int(a[0]/a[1]))
+    print(a[0]%a[1])
+
+if __name__=="__main__":
+    main()
