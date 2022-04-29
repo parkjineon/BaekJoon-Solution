31e4db9c494a722035a869dcaab7fe742620f3e6
Park Jineon <79782666+parkjineon@users.noreply.github.com>
2022-04-29 13:10:48 +0900

    Update 윤년.py

--- 단계별로 풀어보기/조건문/3단계/윤년.py
+++ 단계별로 풀어보기/조건문/3단계/윤년.py
@@ -1,6 +1,6 @@
 def leapYear():
   year=int(input())
-  if( (year%4==0 and year%100!=0) or year%400==0):
+  if( (year%4==0 and year%100!=0) or year%400==0): # Python에서는 &&나 ||, ! 안 쓰고, and, or, not으로 쓴다.
     print(1)
   else:
     print(0)
