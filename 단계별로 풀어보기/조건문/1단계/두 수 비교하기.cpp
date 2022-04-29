34acf283aa9a76d82689fbfb76629cbbbc970ac0
Park Jineon <79782666+parkjineon@users.noreply.github.com>
2022-04-29 10:26:43 +0900

    Create 두 수 비교하기.cpp
    두 정수 A와 B가 주어졌을 때, A와 B를 비교

---
+++ 단계별로 풀어보기/조건문/1단계/두 수 비교하기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin>>A>>B;
+    
+    if(A>B){
+        cout<<'>'<<endl;
+    }else{
+        if(A<B){
+            cout<<'<'<<endl;
+        } else{
+            cout<<"=="<<endl; #==은 문자열이므로 ""로 묶는다.
+        }
+    }
+    return 0;
+}
