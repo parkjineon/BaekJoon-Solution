a67d7ef5c1c788159eb5d95fbd62cf71e1fc3544
Park Jineon <79782666+parkjineon@users.noreply.github.com>
2022-05-04 11:44:29 +0900

    Create 별 찍기-2.cpp
    오른쪽을 기준으로 정렬하여 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제

---
+++ 단계별로 풀어보기/반복문/별 찍기-2/별 찍기-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int printStars(int N){
+    
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=N-i; j++){
+            cout<<' ';
+        }
+        
+        for(int j=1; j<=i; j++){
+            cout<<'*';
+        }
+        cout<<"\n";
+    }
+    
+    return 0;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N;
+    cin>>N;
+    
+    printStars(N);
+}
