5ae7361d488faec8d1da412cba229530aaf2d548
Park Jineon <79782666+parkjineon@users.noreply.github.com>
2022-05-04 09:28:08 +0900

    Create N 찍기.cpp
    첫째 줄부터 N번째 줄 까지 차례대로 출력

---
+++ 단계별로 풀어보기/반복문/N 찍기/N 찍기.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int printToN(int n){
+    for(int i=1; i<=n; i++)
+        cout<<i<<'\n';
+
+    return 0;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin>>n;
+    printToN(n);
+    
+    return 0;
+}
