355221c1ce07f5cf0c0b825e6817b38641d2e73f
Park Jineon <79782666+parkjineon@users.noreply.github.com>
2022-05-12 13:01:39 +0900

    Create 평균.cpp

---
+++ 단계별로 풀어보기/1차원 배열/1546/평균.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+double* enterAllGrade(int N, double* grade) {
+
+	for (int i = 0; i < N; i++) //c++ 배열 길이 찾는 법 sizeof(grade)/sizeof(int)
+		cin >> grade[i];
+	
+	return grade;
+}
+
+double findMax(int N, double* grade) {
+	int M = grade[0];
+	for (int i = 1; i < N; i++) {
+		if (M < grade[i])
+			M = grade[i];
+	}
+
+	return M;
+}
+
+double* makeNewGrade(int N, double* grade) {
+
+	int M = findMax(N, grade);
+
+	for (int i = 0; i < N; i++) //sizeof(grade)/sizeof(int)
+		grade[i] = grade[i] / M * 100;
+
+	return grade;
+
+}
+
+double findNewAvg() {
+	int N;
+	double total=0.0;
+	cin >> N;
+	
+	double* grade = new double[N];
+	
+	enterAllGrade(N, grade);
+	makeNewGrade(N, grade);
+
+	for (int i = 0; i < N; i++) //sizeof(grade)/sizeof(int)
+		total += grade[i];
+
+	total = total / double(N);
+
+	return total;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cout << findNewAvg();
+}
