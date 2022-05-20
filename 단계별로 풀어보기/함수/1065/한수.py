23b5fbd1b219c52103a131c9deff440d1667a6a9
Park Jineon <79782666+parkjineon@users.noreply.github.com>
2022-05-20 17:27:53 +0900

    Create 한수.py

---
+++ 단계별로 풀어보기/함수/1065/한수.py
@@ -0,0 +1,21 @@
+
+def countHansu(N):
+    
+    cnt = 0
+    
+    for i in range(1,N+1):
+        same = True
+        while(same)
+            if N/100 == 0:
+                break
+            differ = ((N%1000)-(N%100))/100-((N%100)-(N%10))/10
+            if(differ != ((N%100)-(N%10))/10-(N%10))
+                same = False
+                break;
+        
+        if same == True:
+            cnt++;
+    
+    return cnt
+    
+countHansu(N)
