6d76b3d134a57cc53f93ba035130484d8f31bfa7
Park Jineon <79782666+parkjineon@users.noreply.github.com>
2022-05-24 16:29:47 +0900

    Create 단어의 개수.cpp

---
+++ 단계별로 풀어보기/문자열/1152/단어의 개수.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string word;
+    int num=0;
+    
+    while(cin>>word){
+        num++;
+    }
+    
+    cout<<num;
+}
