34bd27d1acb334ac072d61003efc87805c522715
Park Jineon <79782666+parkjineon@users.noreply.github.com>
2022-05-29 16:39:37 +0900

    Create 달팽이는 올라가고 싶다.py

---
+++ 단계별로 풀어보기/기본 수학1/2869/달팽이는 올라가고 싶다.py
@@ -0,0 +1,13 @@
+import sys
+
+def init():
+    A, B, V = map(int, sys.stdin.readline().split())
+
+    if (V-A) % (A-B) == 0:
+        print(((V-A) // (A-B))+1)
+    else:
+        print(((V-A) // (A-B))+2)
+
+    return 0
+
+init()
